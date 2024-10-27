1711fe8355c543d83b602b53dca46f9ad2f3c0b9
ziadhamada1 <131191742+ziadhamada1@users.noreply.github.com>
2024-10-27 06:56:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let btn =document.querySelector('.up');
+
+window.onscroll = function (){
+if (window.scrollY>=600){
+    btn.style.display='block'; 
+}else{
+    btn.style.display='none'; 
+}
+
+}
+btn.onclick = function () {
+window.scrollTo({
+ left: 0,
+    top: 0 ,
+    behavior:"smooth",
+});
+
+};
